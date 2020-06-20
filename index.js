f77021924e756d4157ae95a44cd55cc557309bac
chavilla <jcharris.villa@gmail.com>
2020-06-20 14:19:03 -0500

    Ajustes

--- index.js
+++ index.js
@@ -22,6 +22,11 @@ app.use('/api/auth', require('./routes/authRouter'));
 app.use('/api/proyectos/', require('./routes/proyectosRouter'));
 app.use('/api/tareas/', require('./routes/tareasRouter'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 //Habilitar express.json
 app.use(express.json({extended:true }));
